94c0634bc9468296947e78bd2ef54ad61382564e
Sasaki-Momoka <momosasa39@gmail.com>
2026-06-03 14:19:58 +0900

    Calculation_fix

---
+++ src/basicSyntax/DataType.java
@@ -0,0 +1,14 @@
+package basicSyntax;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("こんにちは私はGeek君です。");
+		System.out.println(10);
+		System.out.println("20" + "24");
+		System.out.println(3.14);
+		System.out.println(true);
+	}
+
+}
